10226ad2a1b63f575b301b8743e8ed3021767436
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-27 21:23:37 -0300

    comando-migration-0.2

--- database/migrations/2024_06_28_002202_create_product_table.php
+++ database/migrations/2024_06_28_002202_create_product_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('product', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('price', 8,2);
+            $table->integer('stock');
             $table->timestamps();
         });
     }
