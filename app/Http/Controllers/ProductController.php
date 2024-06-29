0973387fa67afab795832ff3feca306b8e6d0d0e
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-28 22:05:37 -0300

    criando-formulario-create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,6 +25,7 @@ class ProductController extends Controller
     public function create()
     {
         //
+        return view('product.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/products', ProductController::class);
+
