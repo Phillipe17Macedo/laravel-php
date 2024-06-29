7b00191236cd995db562df045da337b28e2a5435
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-06-28 21:55:06 -0300

    criando-tabela-view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,33 @@
-<h1>Tabela</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Index</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>Products</h1>
 
-<ul>
-    <li>Nome</li>
-</ul>
-<ul>
-    <li>Descrição</li>
-</ul>
-<ul>
-    <li>Valor</li>
-</ul>
-<ul>
-    <li>Quantidade</li>
-</ul>
-
-@foreach ($products as $product)
-{{$product->name}}
-@endforeach
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->description }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>{{ $product->stock }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
